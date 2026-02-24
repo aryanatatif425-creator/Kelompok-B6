655ee07573a75e17238d82efb3c147afdd042968
hilmanalfarisi2007-oss <hilman.alfarisi2007@gmail.com>
2026-02-24 22:18:40 +0700

    menambahkan fitur baru

--- Week 2/task_manager.py
+++ Week 2/task_manager.py
@@ -1,45 +1,26 @@
-def tambah_tugas(tasks):
-    judul = input("Masukkan judul tugas: ")
+#task_manager.py
+from asyncio import tasks
+from operator import index
 
-    tugas_baru = {
-        "judul": judul,
-        "status": "Belum Selesai"
-    }
-
+def tambah_tugas(tasks, judul):
+    tugas_baru={"judul": judul, "status": "Belum Selesai"}
     tasks.append(tugas_baru)
-    print("Tugas berhasil ditambahkan!")
+    print("Tugas berhasil ditambahkan! ")
 
-    def hapus_tugas(tasks):
-        if len(tasks) == 0:
-            print("Tidak ada tugas")
-            return 
-        index = int(input("Masukkan nomor tugas yang ingin anda hapus: : "))
-        if 0 <= index < len(tasks):
-            tasks.pop(index)
-            print("Tugas berhasil dihapus! ")
-        else:
-            print("Nomor tugas tidak valid !")
+def hapus_tugas(tasks,index):
+    if len(tasks) == 0:
+        print("Tidak ada tugas")
+        return
+    tasks.pop(index)
+    print("Tugas berhasil dihapus!")
 
 
-    def tanai_selesai(tasks):
-        if len(tasks) == 0:
-            print("Tidak ada tugas")
-            return
+def tandai_selesai(tasks, index):
+    if len(tasks) == 0:
+         print("Tidak ada tugas")
+    return
         
-        index = int(input("Masukkan nomer tugas yang ingin anda tandai selesai: "))
+tasks[index]["status"] = "Selesai"
+print("Tugas berhasil ditandai selesai !")
 
-        if 0 <= index < len(tasks):
-            tasks[index]["status"] = "Selesai"
-            print("Tugas berhasil ditandai selesai! ")
-        else:
-            print("Nomor tugas tidak valid !")
-
-        def lihat_tugas(tasks):
-            if len(tasks) == 0:
-                print("Tidak ada tugas")
-            return
-        
-        print("\nDaftar Tugas: ")
-        for i in range(len(tasks)):
-            print(i, "-", tasks[i]["judul"] + " [" , tasks[i]["status"] + "]")
 
