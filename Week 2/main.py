31022280d7a1e12e564c0b2745ecc42e29dcb9a4
aryanatatif425-creator <arya.nata.tif425@polban.ac.id>
2026-02-24 11:48:07 +0700

    masukkin kerangka main ke branch Arya

--- Week 2/main.py
+++ Week 2/main.py
@@ -9,15 +9,15 @@
 def main():
     print("--- [SISTEM] Program TaskMate Dimulai ---")
     
-    # Dummy file_handler belum siap
+    #  Dummy Data file_handler 
     tasks = [
         {"judul": "Tugas PBO", "status": "Belum"},
         {"judul": "Tugas Kalkulus", "status": "Selesai"}
     ]
 
-    #  Buat Main Loop
+    # 3. Buat Main Loop
     while True:
-        # Dummy UI (
+        # Dummy UI 
         print("\n=== MENU UTAMA ===")
         print("1. Tambah Tugas")
         print("2. Lihat Daftar Tugas")
@@ -27,27 +27,30 @@ def main():
         
         pilihan = input("Pilih menu (1-5): ")
 
-        # Kerangka Percabangan (Routing)
+        # 4. Buat Kerangka Percabangan (Routing)
         if pilihan == '1':
             print("\n[LOG] Anda masuk ke fitur TAMBAH TUGAS")
-            
+            # entar kode di bawah ini akan diaktifkan kalau modulnya sudah siap:
+            # judul_baru = input("Masukkan judul: ")
+            # if validator.cek_input_kosong(judul_baru):
+            #     tasks = task_manager.tambah_tugas(tasks, judul_baru)
             
         elif pilihan == '2':
             print("\n[LOG] Anda masuk ke fitur LIHAT TUGAS")
             print(f"[LOG] Data dummy saat ini: {tasks}")
-            
+            # entar diganti dengan: ui.tampilkan_daftar_tugas(tasks)
             
         elif pilihan == '3':
             print("\n[LOG] Anda masuk ke fitur TANDAI SELESAI")
-            
+            # entar diisi logika memanggil task_manager.tandai_selesai()
             
         elif pilihan == '4':
             print("\n[LOG] Anda masuk ke fitur HAPUS TUGAS")
-
+            # entar diisi logika memanggil task_manager.hapus_tugas()
             
         elif pilihan == '5':
             print("\n[LOG] Menyimpan data ke file...")
-         
+            # entar diganti dengan: file_handler.simpan_data("data.txt", tasks)
             print("[LOG] Keluar dari program. Dadah!")
             break
             
